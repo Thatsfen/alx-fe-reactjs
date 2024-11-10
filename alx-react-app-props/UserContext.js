420264e70fe5fb48a457b5be012b54bf4a7c03e1
Thatsfen <149293411+Thatsfen@users.noreply.github.com>
2024-11-10 22:15:12 +0100

    Update UserContext.js

--- alx-react-app-props/UserContext.js
+++ alx-react-app-props/UserContext.js
@@ -4,7 +4,7 @@ import React, { createContext, useContext } from 'react';
 const UserContext = createContext();
 
 export const useUser = () => useContext(UserContext);
-
+ 
 export const UserProvider = ({ children }) => {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
