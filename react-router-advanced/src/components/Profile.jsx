3e58ddeca48fdc0e3ad0cb3d88f8d4235105663a
Thatsfen <149293411+Thatsfen@users.noreply.github.com>
2024-12-03 22:33:42 +0100

    Update Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails';
+import ProfileSettings from './ProfileSettings';
 
 const Profile = () => {
-  return <h2>Welcome to your Profile!</h2>;
+  return (
+    <div>
+      <h2>Profile Page</h2>
+      <nav>
+        <Link to="details">Profile Details</Link> | 
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default Profile;
