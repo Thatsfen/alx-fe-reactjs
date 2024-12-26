42724a2404b578f8a3218cbc65c12fdb33c1fa25
Thatsfen <149293411+Thatsfen@users.noreply.github.com>
2024-12-26 09:20:11 +0100

    Update tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-darkmode: false, 
-    extend: {},
-  },
-  variants: {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], 
+  darkMode: false, 
+  theme: {
     extend: {},
   },
   plugins: [],
