44c9b75da4804cfbd7a1fe5494e11e44f0fa6d9d
Thatsfen <149293411+Thatsfen@users.noreply.github.com>
2025-01-02 01:18:07 +0100

    fi kan we kan

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <header>
+                <h1>GitHub User Search</h1>
+            </header>
+            <main>
+                {/* Add routing or components here */}
+            </main>
+        </div>
+    );
+};
+
+export default App;
+
---
+++ github-user-search/src/components/UserSearch.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const UserSearch = () => {
+    return <div>Search for GitHub users here</div>;
+};
+
+export default UserSearch;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
